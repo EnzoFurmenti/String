159a90b865a251c9f0f0f65b89bda60921a576b0
EnzoFurmenti <ducheenzo@mail.ru>
2016-08-17 15:40:53 +0400

    add files

---
+++ String/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  String
+//
+//  Created by EnzoF on 17.08.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
---
+++ String/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  String
+//
+//  Created by EnzoF on 17.08.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
